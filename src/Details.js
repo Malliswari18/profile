1c4cd7f4db5ab479fb71b6dd93a296fa0e66d24b
MANOJ KUMAR BOGGAVARAPU <manojbogavarapu@MANOJs-Mac-mini.local>
2025-06-22 21:22:28 +0530

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,104 @@
+// Enter all your detials in this file
+// Logo images
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import python from "./assets/techstack/python.png";
+import c from "./assets/techstack/c.png";
+import java from "./assets/techstack/java.png";
+import googlecloud from "./assets/techstack/googlecloud.png";
+import flutter from "./assets/techstack/flutter.png";
+import jupyter from "./assets/techstack/jupyter.png";
+import github from "./assets/techstack/github.png";
+import vscode from "./assets/techstack/vscode.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+ import projectImage2 from "./assets/projects/project2.png";
+// import projectImage3 from "./assets/projects/project3.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+// import projectImage5 from "./assets/projects/project5.jpg";
+// import projectImage6 from "./assets/projects/project6.jpg";
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Malleswari ",
+  tagline: "Tech Enthusiast",
+  img: profile,
+  about: `Motivated B.Tech student in third year with a passion for technology and problem-solving. programming languages like Java and Python. Eager to learn and contribute to innovative projects As an ambitious computer science undergraduate with a passion for development, I am eager to contribute my technical skills and creativity to real-world projects. knowing programming languages such as Python, Java, and C, I have knowledge coursework in data structures, algorithms, and web development. My strong problem-solving abilities and collaborative mindset make me an asset to any development team. I am excited to apply my knowledge and learn from experienced professionals in the industry`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://linkedin.com/in/malliswari-boggavarapu",
+  github: "https://github.com/Malliswari18",
+  previewLink: "https://malliswari18.github.io/editprofile-app/",
+
+};
+
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Engineering-BE",
+   college: "Geethanjali Institute of Science and Technology",
+    Location: "Nellore-524137",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Intermediate",
+    College: `Jagan's Junior college`,
+    Location: "Nellore",
+    Duration: "Aug 2020 - Mar 2022",
+  },
+
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  python: python,
+  c: c,
+  java: java,
+  googlecloud: googlecloud,
+  flutter: flutter,
+  github: github ,
+  jupyter:jupyter,
+  vscode:vscode,
+ 
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Editprofile app",
+    image: projectImage1,
+    description: `I developed a Flutter application as part of a one-week workshop at my college, conducted by APSSDC (Andhra Pradesh State Skill Development Corporation). During this workshop, I gained hands-on experience and practical knowledge in Flutter, a popular open-source UI software development toolkit created by Google. Additionally, I utilized ChatGPT to assist in developing certain features and functionalities of the app, ensuring a seamless user experience. This project showcases my ability to quickly learn new technologies and apply them effectively to create functional and engaging applications.`,
+    techstack: "HTML/CSS, js,dart",
+    githubLink: "https://github.com/Malliswari18",
+    previewLink: "https://malliswari18.github.io/editprofile-app/",
+  },
+  {
+    title: "mental health prediction system",
+    image: projectImage2,
+    description:'I am excited to share our Mental Health Detection System, developed as part of our college techfest on March 7th. This project leverages machine learning to predict mental health risk scores based on various user inputs and provides personalized suggestions and analysis through an interactive web interface built with Streamlit and Flask.',
+    techstack: "python,streamlit,flask",
+    githubLink: "https://github.com/Malliswari18",
+    previewLink: "https://prediction-model-qzbtkfsewcmtcn3otgdx64.streamlit.app/",
+  },
+ 
+
+ 
+ 
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "malliswarimahendra@gmail.com",
+};
